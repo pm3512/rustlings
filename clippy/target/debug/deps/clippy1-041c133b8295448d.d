2b94963d3aedc4f1981b320775a84c236b939e38
pm3512 <aobolens@andrew.cmu.edu>
2022-12-30 02:52:32 -0500

    completed up to options

---
+++ clippy/target/debug/deps/clippy1-041c133b8295448d.d
@@ -0,0 +1,7 @@
+/home/alex/Documents/projects/rust/rustlings/exercises/clippy/target/debug/deps/clippy1-041c133b8295448d.rmeta: clippy1.rs
+
+/home/alex/Documents/projects/rust/rustlings/exercises/clippy/target/debug/deps/clippy1-041c133b8295448d.d: clippy1.rs
+
+clippy1.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
