55a9de94e6f28c5eae40f4b527ab63472076ee0c
pm3512 <aobolens@andrew.cmu.edu>
2022-12-30 09:22:01 -0500

    completed up to quiz 3

--- quiz3.rs
+++ quiz3.rs
@@ -14,15 +14,61 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum Letter {
+    A, B, C, D, E, F
+}
+
+impl Display for  Letter{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub enum LetterSign {
+    Plus, Minus, None
+}
+
+impl LetterSign {
+    fn to_symbol(&self) -> String {
+        match self {
+            LetterSign::Plus => "+".to_string(),
+            LetterSign::Minus => "-".to_string(),
+            LetterSign::None => String::new()
+        }
+    }
+}
+
+impl Display for LetterSign {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_symbol())
+    }
+}
+struct LetterGrade {
+    letter: Letter,
+    sign: LetterSign
+}
+
+impl Display for LetterGrade {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.letter, self.sign)
+    }
+}
+
+pub trait Grade: Display { }
+impl Grade for f32 { }
+impl Grade for LetterGrade { }
+
+pub struct ReportCard<T: Grade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Grade> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -50,7 +96,10 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: LetterGrade {
+                letter: Letter::A,
+                sign: LetterSign::Plus
+            },
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
